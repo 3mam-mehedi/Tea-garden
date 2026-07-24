d739455b61f1f3562d50f1a801aec379833e7e24
3mam-mehedi <imranhasan3955000@gmail.com>
2026-07-25 01:34:07 +0600

    404

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import path from 'path'
+
+const app = express()
+const port = process.env.PORT || 3000
+const __dirname = path.resolve()
+
+app.use(express.static(path.join(__dirname, 'dist')))
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+})
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`)
+})
